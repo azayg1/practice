e2ef067f91eb21ce9385f269af2cb054c7f62d22
Azay Gupta <azayg1@gmail.com>
2018-12-23 14:27:27 +0530

    Update fileA.java

--- fileA.java
+++ fileA.java
@@ -1,1 +1,11 @@
 
+int numCams = Camera.getNumberOfCameras();
+		if(numCams > 0){
+			try{
+				camera = Camera.open(0);
+				camera.startPreview();
+				preview.setCamera(camera);
+			} catch (RuntimeException ex){
+			
+			}
+}
